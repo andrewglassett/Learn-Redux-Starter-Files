9f690d864a04ce752c7f5b3065067c93fe8870d1
Media <media@Medias-Mac-Pro.local>
2017-01-27 08:36:02 -0500

    setup

---
+++ learn-redux/client/components/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { link } from 'react-router';
+
+const Main = React.createClass({
+  render() {
+    return (
+      <div>
+        <h1> Redux City </h1>
+      </div>
+    )
+  }
+});
+
+export default Main;
